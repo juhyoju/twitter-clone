49a7a9e265c84b0bd779c21bf54f6b5b7b63a8af
hj <20125182@hanmail.net>
2021-06-11 23:19:31 +0900

    Finding File URL, Deleting Files

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,4 +1,4 @@
-import { dbService } from "fbase";
+import { dbService, storageService } from "fbase";
 import React, { useState } from "react";
 
 const Nweet = ({ nweetObj, isOwner }) => {
@@ -6,10 +6,10 @@ const Nweet = ({ nweetObj, isOwner }) => {
     const [newNweet, setNewNweet] = useState(nweetObj.text);
     const onDeleteClick = async () => {
         const ok = window.confirm("Are you sure you want to delete this nweet?");
-        console.log(ok);
         if(ok) {
             // delete nweet
             await dbService.doc(`nweets/${nweetObj.id}`).delete();
+            await storageService.refFromURL(nweetObj.attachmentUrl).delete();
         }
     };
     const toggleEditing = () => setEditing((prev) => !prev);
@@ -46,6 +46,9 @@ const Nweet = ({ nweetObj, isOwner }) => {
                 ) : (
                     <>
                         <h4>{nweetObj.text}</h4>
+                        {nweetObj.attachmentUrl && (
+                            <img src={nweetObj.attachmentUrl} width="50px" height="50px" />
+                        )}
                         {isOwner && (
                             <>
                                 <button onClick={onDeleteClick}>Delete Nweet</button>
